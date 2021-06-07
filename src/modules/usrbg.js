71902ab937cdf8bc259ae169cebad6577864cddd
hax4dazy <Uitvinder.gdm@gmail.com>
2021-06-07 15:06:51 +0200

    Adding shrekbadge into the repo

--- src/modules/usrbg.js
+++ src/modules/usrbg.js
@@ -3,4 +3,8 @@ export default [
     "hax4dazy/usrbg-goosemod",
     "782eebef3c361d09d113a2585d360b06e5a82266",
   ],
+  [
+    "hax4dazy/shrek-badge" ,
+    "6ef980372f55bfff2ead0e79ba457d519f67097f",
+  ],
 ];
